c8cdc64123bebc49466f3361aca764a0412244f9
cj <sigamateusz@gmail.com>
2017-04-10 15:29:35 +0200

    Gets file name from terminal, sorts & saves to _sorted file

--- quicksort.py
+++ quicksort.py
@@ -1,4 +1,32 @@
-def quick_sort(array=[12, 4, 5, 6, 7, 3, 1, 15]):
+import sys
+import os
+import time
+
+
+os.system('clear')
+
+
+def open_file(file_name):
+    output_list = []
+    with open(file_name) as f:
+        reader = f.readlines()
+    for line in reader:
+        output_list.append(int(line))
+    return output_list
+
+
+def write_to_file(file_name, sorted_list):
+    new_name = file_name.split('.')[0] + '_sorted.' + file_name.split('.')[1]
+    print(new_name)
+
+    with open(new_name, 'w+') as file:
+        for number in sorted_list:
+            file.write("%s\n" % number)
+
+
+def quick_sort(array=None):
+    if array is None:
+        array = [12, 4, 5, 6, 7, 3, 1, 15]
     less = []
     equal = []
     greater = []
@@ -19,6 +47,18 @@ def quick_sort(array=[12, 4, 5, 6, 7, 3, 1, 15]):
         return array
 
 
-input_list = [54, 26, 93, 17, 77, 31, 44, 55, 20, 4546, 0, 5]
-sorted_list = quick_sort(input_list)
-print(sorted_list)
+def main():
+    try:
+        file_name = sys.argv[1]
+        list_to_sort = open_file(file_name)
+        start_time = time.time()
+        sorted_list = quick_sort(list_to_sort)
+        end_time = time.time() - start_time
+        print('........', end_time, 'sec........')
+        write_to_file(file_name, sorted_list)
+    except IndexError:
+        print('You don\'t give me any file name')
+
+
+if __name__ == '__main__':
+    main()
